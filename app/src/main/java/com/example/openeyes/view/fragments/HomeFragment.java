7013b5629004ebc74cd8953a18b64c0d1b60921d
iman <imanqhavami.80@gmail.com>
2023-08-09 01:52:04 +0330

    Everything before start map.

--- app/src/main/java/com/example/openeyes/view/fragments/HomeFragment.java
+++ app/src/main/java/com/example/openeyes/view/fragments/HomeFragment.java
@@ -1,13 +1,18 @@
 package com.example.openeyes.view.fragments;
 
 import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.openeyes.R;
+import com.example.openeyes.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,9 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+
+        return binding.getRoot();
 
     }
 }
